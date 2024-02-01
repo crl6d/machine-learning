409b9f9b0ca40ad7e1f0e67a4b41f8fbd4b45339
Kirill Kataman <kirkat0512@gmail.com>
2024-02-01 22:42:16 +0100

    commit

--- DBSCAN/main.py
+++ DBSCAN/main.py
@@ -3,14 +3,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from alg import *
 
-# Загрузка данных из CSV-файла
-file_path = "DBSCAN/data-set/california_housing_test.csv"
+# Load the aircraft incident dataset
+file_path = "DBSCAN/data-set/crashes.csv"
 data = pd.read_csv(file_path)
 
-# Теперь data - это DataFrame, содержащий данные из CSV-файла
+# Choose features for clustering (you need to decide which columns to use)
+features = data[['Aboard', 'Fatalities']].values
+# Choose features for 
 
-eps = 100
+
+
+eps = 30
 min_samples = 4
 
-result_labels = dbscan(data.values, eps, min_samples)  # передаем массив NumPy, а не DataFrame
-plot_clusters(data.values, result_labels)  # передаем массив NumPy, а не DataFrame
+result_labels = dbscan(features, eps, min_samples)
+plot_clusters(features, result_labels)
